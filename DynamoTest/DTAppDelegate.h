030e52c66fc920bb84a7cc4970e3988eff39e875
Fjölnir Ásgeirsson <fjolnir@asgeirsson.is>
2012-05-24 10:31:07 +0900

    Initial Commit

---
+++ DynamoTest/DTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DTAppDelegate.h
+//  DynamoTest
+//
+//  Created by Fjölnir Ásgeirsson on 5/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DynamoTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DynamoTest
+//
+//  Created by Fjölnir Ásgeirsson on 5/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DTAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DTAppDelegate class]));
+    }
+}
